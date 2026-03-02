bed2cf65fde37063b179ebae7c48166793f76202
Krien <uwe.krien@ifam.fraunhofer.de>
2026-03-02 19:51:52 +0100

    Clean up example

--- examples/f_heat_import.py
+++ examples/f_heat_import.py
@@ -4,8 +4,7 @@ from matplotlib import pyplot as plt
 
 from oemof.eesyplan.importer.heat_demand import import_heat_demand_f_heat
 
-df = import_heat_demand_f_heat(
-    Path(Path.home(), "Downloads", "Uebung_FHeat_Barntrup.zip"), "Netz1"
-)
+zip_file = Path(Path.home(), "Downloads", "Uebung_FHeat_Barntrup.zip")
+df = import_heat_demand_f_heat(zip_file, "Netz1")
 df.plot()
 plt.show()
